50b9ccb0aaea8b001e24092dd8ee577894ce455e
imabhi1216 <105545947+imabhi1216@users.noreply.github.com>
2023-02-16 14:30:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,41 @@
 import streamlit as st
 from gtts import gTTS
-from gtts import *
 import PyPDF2
 from tqdm import tqdm
 
+# Set title of the app
+st.title('Book Reader')
 
-st.title('Book Translator and Reader')
+# Allow user to upload a PDF file
 pdf_file = st.file_uploader("Upload Book PDF", type=["pdf"])
 
 def pdf_to_text(pdf_file):
     whole = ''
+    # Read the PDF file using PyPDF2
     pdfreader= PyPDF2.PdfReader(pdf_file)
-    # choice2 = st.sidebar.selectbox("Translated_Language",lang)
+    # Get the number of pages in the PDF file
     pages=len(pdfreader.pages)
     
-    page = st.slider('Give the range of the Pages you want to access',0, pages, (25, 27   ))
-    # [start_page,end_page] = input("Enter the start page number give a space and enter end page number").split(" ")
+    # Allow user to select a range of pages to extract text from
+    page = st.slider('Give the range of the Pages you want to access',0, pages, (25, 27 ))
+    
+    # Loop through each page in the range and extract the text using PyPDF2
     for num in tqdm(range(int(page[0]),int(page[1]))):
         Page=pdfreader.pages[num]
         text=Page.extract_text()
-        
-    whole+=text   
+        whole += text   
 
-    myobj = gTTS(text=text, lang='en',tld='co.in', slow=False) 
- 
+    # Convert the extracted text to speech using gTTS
+    myobj = gTTS(text=whole, lang='en',tld='co.in', slow=False) 
+    # Save the audio file to disk
     myobj.save("test.wav") 
-    return "test.wav",text
+    # Return the path to the audio file and the extracted text
+    return "test.wav",whole
 
-
-
+# If a PDF file has been uploaded, extract the text and convert it to speech
 if pdf_file is not None:
-
-    A,T = pdf_to_text(pdf_file)
-    st.write(T)
-    st.audio(A)
+    audio_path, extracted_text = pdf_to_text(pdf_file)
+    # Display the extracted text
+    st.write(extracted_text)
+    # Allow user to listen to the text as speech
+    st.audio(audio_path)
